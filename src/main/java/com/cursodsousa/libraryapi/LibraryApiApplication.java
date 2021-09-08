a33b6d75ee8aeb4be9d786aa8c53e1052201eb34
atomita <atomita@uolinc.com>
2021-09-08 17:17:27 -0300

    ajuste do projeto

--- src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
@@ -1,23 +1,17 @@
 package com.cursodsousa.libraryapi;
 
-import com.cursodsousa.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
+
 	@Bean
-	public ModelMapper modelMapper(){
+	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
